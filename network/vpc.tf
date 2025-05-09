5a2fddc6d544551c1be8bd91f2c13ade96090b57
Vitor Gransotto <gransotto2002@icloud.com>
2025-05-08 22:20:58 -0400

    fix: availabity zones for subnets

--- network/vpc.tf
+++ network/vpc.tf
@@ -10,13 +10,13 @@ resource "aws_subnet" "public_subnet" {
   vpc_id = aws_vpc.main.id
   cidr_block = "10.0.0.0/24"
 
-  availability_zone = "us-east-1"
+  availability_zone = "us-east-1a"
 }
 
 resource "aws_subnet" "private_subnet" {
   vpc_id = aws_vpc.main.id
   cidr_block = "10.0.1.0/24"
-  availability_zone = "us-east-1"
+  availability_zone = "us-east-1a"
 
   map_public_ip_on_launch = false
 }
