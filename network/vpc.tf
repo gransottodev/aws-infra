06786e65bfadb10bcf9cdac55e045b435d6e4687
Vitor Gransotto <gransotto2002@icloud.com>
2025-05-08 22:18:17 -0400

    fix: public subnet ip

--- network/vpc.tf
+++ network/vpc.tf
@@ -8,14 +8,14 @@ resource "aws_vpc" "main" {
 
 resource "aws_subnet" "public_subnet" {
   vpc_id = aws_vpc.main.id
-  cidr_block = "10.0.0.1/24"
+  cidr_block = "10.0.0.0/24"
 
   availability_zone = "us-east-1"
 }
 
 resource "aws_subnet" "private_subnet" {
   vpc_id = aws_vpc.main.id
-  cidr_block = "10.0.2.0/24"
+  cidr_block = "10.0.1.0/24"
   availability_zone = "us-east-1"
 
   map_public_ip_on_launch = false
